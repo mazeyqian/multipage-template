e7d65e3fb0eeb322ebc2bba9d2a279f49c959704
mazey <mazey@mazey.net>
2017-10-25 23:26:53 +0800

    data避免直接赋值；解决办法：通过function返回数值，实例化的过程中都是新的数据。

--- src/main.js
+++ src/main.js
@@ -2,11 +2,29 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 
+var myHeaderChild = {
+  template: '<p>i am my header child </p>'
+}
+
+var myHeader = {
+  template: '<p><my-header-child></my-header-child>this {{}} is my header</p>',
+  components: {
+    'my-header-child': myHeaderChild
+  },
+  data: function () {
+    return {
+      f: 1,
+      d: 2
+    }
+  }
+}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<p>hello, world {{ word}}</p>',
   data: {
-    word: 'hello world'
+    word: 'hello world2'
+  },
+  components: {
+    'my-header': myHeader
   }
 })
