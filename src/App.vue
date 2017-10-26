6af0a5e67d859e81b745b03ecf48ecc10766dd26
mazey <mazey@mazey.net>
2017-10-26 15:05:08 +0800

    引入组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <componentA></componentA>
     <ul>
       <li v-for="(value, key) in objList"> {{ key + '-' + value }} </li>
     </ul>
@@ -7,7 +8,9 @@
 </template>
 
 <script>
+import componentA from './components/a.vue'
 export default {
+  components: {componentA},
   data () {
     return {
       hello: '<strong>world</strong>',
---
+++ src/components/a.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        hello: 'i am component a'
+      }
+    }
+  }
+</script>
+
+<style>
+  
+</style>
