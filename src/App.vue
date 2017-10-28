b9e26c4a6269d87f2141edf7bbb93d1025116858
mazey <mazey@mazey.net>
2017-10-28 18:10:44 +0800

    computed 计算属性。

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,16 @@
 <template>
   <div>
-    {{ myVal }}
-    <v-select :options="options"></v-select>
+    <input type="text" v-model="myValue">
+    {{ myValueWithoutNum }}
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import comA from './components/a'
-import vSelect from './components/vSelect'
 export default {
-  components: {
-    comA,
-    vSelect
-  },
   data () {
     return {
-      hello: 'world',
-      myVal: 0,
-      options: [
-        {
-          name: 'apple',
-          val: 0
-        },
-        {
-          name: 'banana',
-          val: 1
-        },
-        {
-          name: 'orange',
-          val: 2
-        }
-      ],
+      myValue: '',
+      hello: '<strong>world</strong>',
       list: [
         {
           name: 'apple',
@@ -44,22 +23,9 @@ export default {
       ]
     }
   },
-  methods: {
-    addItem () {
-      Vue.set(this.list, 1, {
-        name: 'pinaapple',
-        price: 256
-      })
-      this.linkCss.color = 'blue'
-    },
-    toggle () {
-      this.isPartA = !this.isPartA
-    },
-    onKeyDown () {
-      console.log('on key down')
-    },
-    onComaMyEvent (parmfromA) {
-      console.log('onComaMyEvent+' + parmfromA)
+  computed: {
+    myValueWithoutNum: function () {
+      return this.myValue + ' end'
     }
   }
 }
